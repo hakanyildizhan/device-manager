395e9bc6fdcfdf455e51a58c87ef3215e65aaf1a
Hakan Yıldızhan <yildizhanhakan@outlook.com>
2020-04-06 10:16:40 +0300

    Allow only a single instance of the client application to run simultaneously

--- src/Client/DeviceManager.Client.TrayApp/App.xaml.cs
+++ src/Client/DeviceManager.Client.TrayApp/App.xaml.cs
@@ -6,6 +6,7 @@ using System.Configuration;
 using System.Data;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -16,14 +17,24 @@ namespace DeviceManager.Client.TrayApp
     /// </summary>
     public partial class App : Application
     {
+        static Mutex mutex = new Mutex(true, "{D945067F-CF69-4850-8E06-3DDA6625BA76}");
+
         protected override void OnStartup(StartupEventArgs e)
         {
-            base.OnStartup(e);
-            SetupApplication();
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                base.OnStartup(e);
+                SetupApplication();
 
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Visibility = Visibility.Hidden;
-            mainWindow.Show();
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Visibility = Visibility.Hidden;
+                mainWindow.Show();
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("The application is already running.");
+            }
         }
 
         private void SetupApplication()
