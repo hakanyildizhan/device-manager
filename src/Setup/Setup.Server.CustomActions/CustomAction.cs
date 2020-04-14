bded484baf19b70c224a0ff115aed131782d5d80
Hakan Yildizhan <hakan.yildizhan@siemens.com>
2020-04-14 09:43:42 +0300

    Server installer that installs files, IIS site and Database & grants permissions to App pool. - Commented out uninstall scripts to instead run them on afterfinalize, since Database jobs seem to take precedence over IIS, so the IIS login cannot be created on DB. - Needs work on uninstall: DB & IIS site (if new site) & App pool do not get deleted. IIS site gets deleted only if it is in a virtual directory under Default Web Site. User choices might need to be saved to registry & read from there during uninstall to fix it.

--- src/Setup/Setup.Server.CustomActions/CustomAction.cs
+++ src/Setup/Setup.Server.CustomActions/CustomAction.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
 using Microsoft.Deployment.WindowsInstaller;
 
@@ -60,32 +62,13 @@ namespace DeviceManager.Setup.Server.CustomActions
         {
             session.Log("Begin TestConnection");
 
-            string authMode = session["DB_AUTHENTICATIONMODE"];
-            string server = session["DB_SERVER"];
-            string user = session["DB_USER"];
-            string password = session["DB_PASSWORD"];
+            string connString = GenerateConnectionString(session);
 
-            bool authModeIsWindows = authMode == "0";
-
-            // validate inputs
-            if (string.IsNullOrEmpty(server) || 
-                (!authModeIsWindows && 
-                  (string.IsNullOrEmpty(user) || string.IsNullOrEmpty(password))))
+            if (string.IsNullOrEmpty(connString))
             {
                 session.Log("Invalid input for TestConnection");
                 session["DB_SERVER_OK"] = "0";
                 return ActionResult.Success;
-            }
-
-            string connString = $"Server={server};database=master;";
-
-            if (authModeIsWindows)
-            {
-                connString += "Integrated Security=SSPI;";
-            }
-            else
-            {
-                connString += $"User Id={user};Password={password};";
             }
 
             using (SqlConnection connection = new SqlConnection(connString))
@@ -103,6 +86,83 @@ namespace DeviceManager.Setup.Server.CustomActions
                     session["DB_SERVER_OK"] = "0";
                     return ActionResult.Success;
                 }
+            }
+        }
+
+        [CustomAction]
+        public static ActionResult GrantDatabasePermissions(Session session)
+        {
+            //System.Diagnostics.Debugger.Launch();
+            session.Log("Begin GrantDatabasePermissions");
+            string connString = GenerateConnectionString(session);
+
+            if (string.IsNullOrEmpty(connString))
+            {
+                session.Log("Invalid input for GrantDatabasePermissions");
+                return ActionResult.Success;
+            }
+
+            string sqlCommand = GetEmbeddedSQLFileContents();
+
+            using (SqlConnection connection = new SqlConnection(connString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand(sqlCommand, connection);
+                    cmd.ExecuteNonQuery();
+                    session.Log("Granted database permissions successfully");
+                    return ActionResult.Success;
+                }
+                catch (SqlException)
+                {
+                    session.Log("Could not grant database permissions");
+                    return ActionResult.Success;
+                }
+            }
+        }
+
+        private static string GenerateConnectionString(Session session)
+        {
+            string authMode = session["DB_AUTHENTICATIONMODE"];
+            string server = session["DB_SERVER"];
+            string user = session["DB_USER"];
+            string password = session["DB_PASSWORD"];
+
+            bool authModeIsWindows = authMode == "0";
+
+            // validate inputs
+            if (string.IsNullOrEmpty(server) ||
+                (!authModeIsWindows &&
+                  (string.IsNullOrEmpty(user) || string.IsNullOrEmpty(password))))
+            {
+                return string.Empty;
+            }
+
+            string connString = $"Server={server};database=master;";
+
+            if (authModeIsWindows)
+            {
+                connString += "Integrated Security=SSPI;";
+            }
+            else
+            {
+                connString += $"User Id={user};Password={password};";
+            }
+
+            return connString;
+        }
+
+        private static string GetEmbeddedSQLFileContents()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "DeviceManager.Setup.Server.CustomActions.Resources.Script_v1.0.0.0_User.sql";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
+                return result;
             }
         }
     }
