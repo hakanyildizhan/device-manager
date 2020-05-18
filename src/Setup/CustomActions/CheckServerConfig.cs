50fb5fc7740202cc3c33b4f2e349a6597af28c7c
Hakan Yildizhan <hakan.yildizhan@siemens.com>
2020-05-18 14:10:37 +0300

    Port 80 virtual directory handling on client setup #25

---
+++ src/Setup/CustomActions/CheckServerConfig.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Deployment.WindowsInstaller;
+using System.IO;
+
+namespace DeviceManager.Setup.CustomActions
+{
+    public partial class CustomActions
+    {
+        /// <summary>
+        /// Checks if the user already has a config file that contains a valid server URL.
+        /// If so, sets TESTRESULT to "1" and sets the SERVERADDRESS property.
+        /// </summary>
+        /// <param name="session"></param>
+        /// <returns></returns>
+        [CustomAction]
+        public static ActionResult CheckServerConfig(Session session)
+        {
+            //System.Diagnostics.Debugger.Launch();
+            session.Log("CheckServerConfig CustomAction started");
+
+            // if file does not exist, do nothing
+            if (!File.Exists(_configFile))
+            {
+                return ActionResult.Success;
+            }
+
+            var getServerSettingResult = GetServerSetting();
+            session.Log(getServerSettingResult.Message);
+
+            string serverAddress = getServerSettingResult.Result;
+
+            // server setting does not exist. return
+            if (string.IsNullOrEmpty(serverAddress))
+            {
+                session["TESTRESULT"] = "0";
+                return ActionResult.Success;
+            }
+
+            // test the server validity
+            var serverValidityResult = Utility.TestServerValidity(serverAddress);
+            session.Log(serverValidityResult.Message);
+            session["TESTRESULT"] = serverValidityResult.Success ? "1" : "0";
+            session["SERVERADDRESS"] = serverValidityResult.Success ? serverAddress : "";
+            return ActionResult.Success;
+        }
+    }
+}
